2a19d4ceb808e811aac38da3a2f18ac3f3f5cfc1
somrat <jafaruddin875@gmail.com>
2023-04-04 15:06:37 +0600

    import outlet and call it

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <h1>This is Home</h1>
+        <Outlet></Outlet>
+    </div>
   )
 }
