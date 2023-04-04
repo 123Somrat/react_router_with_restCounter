69a5c2ea315a097c427ea5974a3fe4485218617a
somrat <jafaruddin875@gmail.com>
2023-04-04 14:31:20 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+        <h1 className='text-5xl text-purple-300 underline text-center'>hellow world</h1>
+    </div>
+  )
+}
+
+export default App
